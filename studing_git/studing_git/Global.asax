fed3f055bbf6cd430641afc8d4ac85bb93f0035f
Abeare <abeare21@hotmail.com>
2016-03-13 08:12:48 +0300

    chang in the index header text

---
+++ studing_git/studing_git/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="studing_git.MvcApplication" Language="C#" %>
---
+++ studing_git/studing_git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(studing_git.Startup))]
+namespace studing_git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
