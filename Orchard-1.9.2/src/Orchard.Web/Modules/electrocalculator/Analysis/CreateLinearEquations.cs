9aef9dee715a92a6a185082c78a6d4eb3e68d932
Abeare <abeare21@hotmail.com>
2016-04-14 03:08:24 +0300

    calculate resistor,capacitor,inductor is ready

--- Orchard-1.9.2/src/Orchard.Web/Modules/electrocalculator/Analysis/CreateLinearEquations.cs
+++ Orchard-1.9.2/src/Orchard.Web/Modules/electrocalculator/Analysis/CreateLinearEquations.cs
@@ -69,65 +69,19 @@ namespace electrocalculator.Analysis
             form_s();
 
             //localanlysis
-            //Servicegauss.Complexdata[] ccn = ArrayTransform.manyToOne(w, _nodeInCome);
-            //w = ArrayTransform.oneToMany(ccn, _nodeInCome);
-            //GaussMethod.gauss_c(w, _nodeInCome);
+            Servicegauss.Complexdata[] ccn = ArrayTransform.manyToOne(w, _nodeInCome);
+            w = ArrayTransform.oneToMany(ccn, _nodeInCome);
+            GaussMethod.gauss_c(w, _nodeInCome);
 
-            //int km = kmInput;
-            //int kp = kpInput;
-            //double kum, kua;
-            //Complex ku, ri;
-            //ri = w[0, 1] - w[0, 0];
-            //ku = (w[0, kp] - w[0, km]) / ri;
-            //kum = Complex.abs(ku);
-            //kua = Complex.arg(ku) * (180 / 3.1416);
+            int km = kmInput;
+            int kp = kpInput;
+            double kum, kua;
+            Complex ku, ri;
+            ri = w[0, 1] - w[0, 0];
+            ku = (w[0, kp] - w[0, km]) / ri;
+            kum = Complex.abs(ku);
+            kua = Complex.arg(ku) * (180 / 3.1416);
 
-
-
-
-
-
-
-            //var myBinding = new BasicHttpBinding();
-            //var myEndpoint = new EndpointAddress("http://localhost:7674/Service1.svc");
-            //var myChannelFactory = new ChannelFactory<Servicegauss.IService1>(myBinding, myEndpoint);
-
-            //Servicegauss.IService1 client = null;
-
-            //try
-            //{
-            //    client = myChannelFactory.CreateChannel();
-            //    Servicegauss.Complexdata[] send = ArrayTransform.manyToOne(w, _nodeInCome);
-            //    Servicegauss.Complexdata[] respons =client.gauss_c(send,_nodeInCome);
-
-            //    w = ArrayTransform.oneToMany(respons, _nodeInCome);
-            //    ((ICommunicationObject)client).Close();
-            //}
-            //catch
-            //{
-            //    if (client != null)
-            //    {
-            //        ((ICommunicationObject)client).Abort();
-            //    }
-            //}
-
-            //using (var client = new Servicegauss.Service1Client(binding, address))
-            //{
-            //    Servicegauss.Complexdata[] send = ArrayTransform.manyToOne(w, _nodeInCome);
-            //    Servicegauss.Service1Client gaus = new Servicegauss.Service1Client();
-            //    Servicegauss.Complexdata[] respons = gaus.gauss_c(send, _nodeInCome);
-            //    w = ArrayTransform.oneToMany(respons, _nodeInCome);
-            //}
-
-
-            //int km = kmInput;
-            //int kp = kpInput;
-            //double kum, kua;
-            //Complex ku, ri;
-            //ri = w[0, 1] - w[0, 0];
-            //ku = (w[0, kp] - w[0, km]) / ri;
-            //kum = Complex.abs(ku);
-            //kua = Complex.arg(ku) * (180 / 3.1416);
 
 
 
---
+++ Orchard-1.9.2/src/Orchard.Web/Modules/electrocalculator/codeForTempSave.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace electrocalculator
+{
+    public class codeForTempSave
+    {
+
+        //using (var client = new Servicegauss.Service1Client(binding, address))
+        //{
+        //    Servicegauss.Complexdata[] send = ArrayTransform.manyToOne(w, _nodeInCome);
+        //    Servicegauss.Service1Client gaus = new Servicegauss.Service1Client();
+        //    Servicegauss.Complexdata[] respons = gaus.gauss_c(send, _nodeInCome);
+        //    w = ArrayTransform.oneToMany(respons, _nodeInCome);
+        //}
+
+
+        //int km = kmInput;
+        //int kp = kpInput;
+        //double kum, kua;
+        //Complex ku, ri;
+        //ri = w[0, 1] - w[0, 0];
+        //ku = (w[0, kp] - w[0, km]) / ri;
+        //kum = Complex.abs(ku);
+        //kua = Complex.arg(ku) * (180 / 3.1416);
+
+
+
+
+
+
+
+
+
+
+
+        //var myBinding = new BasicHttpBinding();
+        //var myEndpoint = new EndpointAddress("http://localhost:7674/Service1.svc");
+        //var myChannelFactory = new ChannelFactory<Servicegauss.IService1>(myBinding, myEndpoint);
+
+        //Servicegauss.IService1 client = null;
+
+        //try
+        //{
+        //    client = myChannelFactory.CreateChannel();
+        //    Servicegauss.Complexdata[] send = ArrayTransform.manyToOne(w, _nodeInCome);
+        //    Servicegauss.Complexdata[] respons =client.gauss_c(send,_nodeInCome);
+
+        //    w = ArrayTransform.oneToMany(respons, _nodeInCome);
+        //    ((ICommunicationObject)client).Close();
+        //}
+        //catch
+        //{
+        //    if (client != null)
+        //    {
+        //        ((ICommunicationObject)client).Abort();
+        //    }
+        //}
+
+
+    }
+}
